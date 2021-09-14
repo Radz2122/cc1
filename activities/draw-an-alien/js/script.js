b2444b84b6421ba7957a19b24fb9c57fd5582bd1
Radz2122 <70171361+Radz2122@users.noreply.github.com>
2021-09-13 20:02:41 -0400

    Activity alien done

--- activities/draw-an-alien/js/script.js
+++ activities/draw-an-alien/js/script.js
@@ -21,6 +21,26 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(800,800);
+  background('#222222');
+  noStroke();
+
+  fill(144,238,144);
+  circle(400, 400, 500);
+  translate(140, 45);
+  scale(2.5);
+  triangle(30, 75, 58, 20, 86, 75);
+  translate(90,0);
+  triangle(30, 75, 58, 20, 86, 75);
+
+  fill(0,0,0);
+  ellipse(56, 120, 45, 45);
+  ellipse(-20, 120, 45, 45);
+  fill(244,194,194);
+strokeWeight(4);
+stroke(51);
+arc(5, 180, 50, 50, 0, PI + QUARTER_PI, CHORD);
+
 
 }
 
