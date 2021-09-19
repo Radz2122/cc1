850aa4a3ede1f6176a7368a34bc5186534cf572f
Radz2122 <70171361+Radz2122@users.noreply.github.com>
2021-09-18 21:05:07 -0400

    Continuation

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -1,14 +1,19 @@
 /**
-Title of Project
-Author Name
+Exercise 1 - I like to move it
+Radhika Patel
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This is the first exercise in which mouvement,mouse positioning, colors, shapes and the map()+ constrain() functions are explored.
 */
 
 "use strict";
 
-
+//object for background color
+let bg={
+  r:173,
+  g:216,
+  b:230
+};
+let rotationSpeed=2;
 /**
 Description of preload
 */
@@ -21,6 +26,7 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(800,800);
 
 }
 
@@ -29,5 +35,23 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(bg.r,bg.g,bg.b);
 
+  // fill();
+  polygon(100, 100, 120, 10);
+
+  line(30, 20, 85, 75);
+}
+
+//Function to create polygon,
+//taken from P5 reference website: https://p5js.org/examples/form-regular-polygon.html
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
