1edb0541c5ea9436ccb27d528f58bc85a40df47d
Radz2122 <70171361+Radz2122@users.noreply.github.com>
2021-10-19 17:20:53 -0400

    setup and character

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,19 +1,32 @@
 /**
-Title of Project
-Author Name
+Project 1
+Radhika Patel
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This is my first project, a spy simulation game
 */
 
 "use strict";
-
+//object that represents the player
+let user = {
+  image: undefined,
+  x: 0,
+  y: 0,
+  size: 200,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0,
+  acceleration: 0.1,
+  deceleration: 0,
+  maxSpeed: 8
+};
 
 /**
 Description of preload
 */
 function preload() {
-
+  // SOURCE------- https://opengameart.org/content/animated-top-down-survivor-player
+  user.image = loadImage("assets/images/survivor-move_flashlight_0.png");
 }
 
 
@@ -21,7 +34,7 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+createCanvas(windowWidth, windowHeight);
 }
 
 
@@ -29,5 +42,7 @@ function setup() {
 Description of draw()
 */
 function draw() {
-
+  background(255, 255, 255);
+  imageMode(CENTER);
+  image(user.image, user.x, user.y, user.size, user.size);
 }
