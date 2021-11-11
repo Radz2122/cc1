23ae5225e69cdb70ca11ff64e900d3fded516412
Radz2122 <70171361+Radz2122@users.noreply.github.com>
2021-11-11 18:21:10 -0500

    P2- created the card grid and attempted to center it

--- projects/project 2/prototype/js/Card.js
+++ projects/project 2/prototype/js/Card.js
@@ -2,17 +2,15 @@ class Card{
   constructor(x,y){
     this.x=x;
     this.y=y;
-    this.width=300;
-    this.height=500;
+    this.width=150;
+    this.height=200;
   }
-}
 
-
-function display(){
-  push();
-  fill(255);
-  noStroke();
-  rectMode(CENTER);
-  rect(this.x, this.y, this.width, this.height);
-  pop();
+   display(){
+    push();
+    fill(255);
+    rectMode(CENTER);
+    rect(this.x, this.y, this.width, this.height);
+    pop();
+  }
 }
--- projects/project 2/prototype/js/script.js
+++ projects/project 2/prototype/js/script.js
@@ -2,25 +2,24 @@
 Project 2
 Radhika Patel
 
-This is my final project, a matchmaking memory game with a twist 
+This is my final project, a matchmaking memory game with a twist
 */
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
+//starts off the game with the game FOR NOW
+let state = `game`;
+let cards = [];
+// How many rows and columns in the grid?
+let rows = 3;
+let cols = 4;
 
 /**
 Description of setup
 */
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
+  createCardGrid();
 }
 
 
@@ -28,5 +27,27 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
+  if (state === `game`) {
+    game();
+  }
+}
 
+//displays the game assets
+function game(){
+  for (let i = 0; i < cards.length; i++) {
+    let card= cards[i];
+    card.display();
+  }
+}
+
+//displays the cards in a grid
+// code inspired by Pippin's grid: https://editor.p5js.org/pippinbarr/sketches/Xq3qsbQWA
+function createCardGrid(){
+  for (let c=0; c < cols; c++) {
+    for (let r =0 ;r< rows ;r++) {
+      let card = new Card(c * 280 + windowWidth/3.5, r * 280 + windowHeight/4);
+      cards.push(card);
+    }
+  }
 }
