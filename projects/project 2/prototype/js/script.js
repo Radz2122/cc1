788dd3a890214c8160abf594da0ce55e5eb38ecf
Radz2122 <70171361+Radz2122@users.noreply.github.com>
2021-11-11 22:01:24 -0500

    array successfully copied

--- projects/project 2/prototype/js/script.js
+++ projects/project 2/prototype/js/script.js
@@ -9,11 +9,14 @@ This is my final project, a matchmaking memory game with a twist
 
 //starts off the game with the game FOR NOW
 let state = `game`;
+//array that contains the first set of cards
 let cards = [];
+//array that will contain the copy of the first array to make pairs
+let copyCards=[];
 
 // How many rows and columns in the grid?
 let rows = 3;
-let cols = 4;
+let cols = 2;
 
 let patternChoice;
 let possiblePatterns=["pattern1","pattern2","pattern3","pattern4","pattern5","pattern6"];
@@ -43,6 +46,10 @@ function game(){
     let card= cards[i];
     card.display();
   }
+  for (let i = 0; i < copyCards.length; i++) {
+    let copiedCard= copyCards[i];
+    copiedCard.display();
+  }
 }
 
 //displays the cards in a grid
@@ -52,15 +59,17 @@ function createCardGrid(){
     for (let r =0 ;r< rows ;r++) {
       //selectiing a possible pattern at random
       const patternChoice = Math.floor(Math.random() * possiblePatterns.length);
-      console.log(patternChoice);
       //creating cards and pushing them into their array
       // the multiplied value is the distance between the cards
       //the addded value is the position of the entire card grid
-      let card = new Card(c * 280 + windowWidth/3.5, r * 280 + windowHeight/4,patternChoice);
+      //had to play around with the numbers to find the correct ones....
+      let card = new Card(c * 280 + windowWidth/3.5, r * 280 + windowHeight/4,possiblePatterns[patternChoice]);
       cards.push(card);
       possiblePatterns.splice(patternChoice,1);
-      console.log(possiblePatterns);
+      arrayCopy(cards,0,copyCards,0,cards.length);
 
     }
   }
+    console.log(cards);
+    console.log(copyCards);
 }
